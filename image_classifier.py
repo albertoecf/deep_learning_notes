f0fb61f162e420f6bc9f07e702ec81f180cf778f
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-16 08:26:31 +0100

    Prepare to run, split, and calculate accuracy

--- image_classifier.py
+++ image_classifier.py
@@ -1,10 +1,13 @@
 #%%
 from PIL import Image
-
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 import os
+
+from keras.models import Sequential
+from keras.layers import Activation, Conv2D, Dense, Flatten, MaxPool2D
+from sklearn.model_selection import train_test_split
 
 def images_to_array(folder,name):
     for imgage in os.listdir(folder):
@@ -23,7 +26,6 @@ def show_image(index):
 x = [] # images
 y = [] # categories 
 
-print(type(x))
 folder_burger = 'burger_pictures'
 folder_box = 'boxes_pictures'
 
@@ -40,13 +42,8 @@ x = ((x - 127.5)/127.5)
 
 show_image(90)
 
-# %%
-from keras.layers import Activation, Conv2D, Dense, Flatten, MaxPool2D
-from keras.models import Sequential
-# %%
 # Create the Architecture for the CNN
 model = Sequential()
-
 
 model.add(Conv2D(32, (5,5), padding='same',activation='relu', input_shape=(100,100,3))) # we resize imagenes -> (100,100). rbg = 3
 model.add(MaxPool2D(pool_size=(2,2))) # MaxPool -> max value pooling. we could use : min, average..
@@ -57,6 +54,31 @@ model.add(MaxPool2D(pool_size=(2,2)))
 model.add(Conv2D(100, (5,5), padding='same',activation='relu')) 
 model.add(MaxPool2D(pool_size=(2,2)))
 
+model.add(Flatten())
+model.add(Dense(124))
+model.add(Activation('relu'))
+model.add(Dense(2))
+model.add(Activation('sigmoid'))
 
+model.summary()
+# %%
+# Prepare to run
+x_train, x_test , y_train, y_test = train_test_split(x,y, test_size=0.1) # split train and test
+# %%
+from keras.optimizers import Adam
+optimizer_adam = Adam(lr=0.001) #learning rate
+model.compile(optimizer=optimizer_adam, loss='binary_crossentropy',metrics=['accuracy'])
+h = model.fit(x_train, y_train, batch_size=10, epochs=20, validation_data=(x_test,y_test))
+model.save('burger_vs_boxex_CNN.h5')
 
+plt.plot(h.history['accuracy'], label='train')
+plt.plot(h.history['val_accuracy'], label='test')
+plt.title('Model accuracy')
+plt.xlabel('Epoch Number')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+model_score = model.evaluate(x_test, y_test, verbose=0)
+print('Test accuracy : {}'.format(model_score[1]))
 # %%
