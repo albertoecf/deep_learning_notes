1b7a4a9ae4b8f6200445bf8c1bfa01fac874cb16
aecf <81099969+albertoecf@users.noreply.github.com>
2022-12-16 07:45:43 +0100

    feat: Data preparation for NN and reorganize file

--- image_classifier.py
+++ image_classifier.py
@@ -6,15 +6,6 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 import os
 
-x = [] # images
-y = [] # categories 
-
-print(type(x))
-folder_burger = 'burger_pictures'
-folder_box = 'boxes_pictures'
-
-name_encoded = {"burger":0, "box":1 }
-
 def images_to_array(folder,name):
     for imgage in os.listdir(folder):
         loaded_image = Image.open(os.path.join(folder, imgage))
@@ -29,10 +20,25 @@ def show_image(index):
     plt.show()
     print(y[index])
 
+x = [] # images
+y = [] # categories 
+
+print(type(x))
+folder_burger = 'burger_pictures'
+folder_box = 'boxes_pictures'
+
+name_encoded = {"burger":0, "box":1 }
+
 images_to_array(folder_box, "box")
 images_to_array(folder_burger, "burger")
 
 y = to_categorical(y, num_classes=2) #one hot encoding
-
+x = np.array(x) # convert to numpy array 
 show_image(0)
-#%%
+#Normalize data-> Now data is 0-255 , we need between -1 , 1 
+x = ((x - 127.5)/127.5) 
+
+show_image(90)
+
+# %%
+# Create the  Architecture
